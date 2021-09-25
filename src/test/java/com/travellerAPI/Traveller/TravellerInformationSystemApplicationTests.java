5dddd9c73e8ad0f2696964258554e580f76db594
Harish Chandrasekaran <harishchandrasekaran@harishs-mbp.myfiosgateway.com>
2021-09-25 15:38:49 -0400

    Initialised project

---
+++ src/test/java/com/travellerAPI/Traveller/TravellerInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travellerAPI.Traveller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellerInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
